c7ba997ca80ad37cadabc2f95811d48de2aace60
pankajnbot <pankajnegi@botaiml.com>
2021-06-13 13:27:29 +0530

    minor changes

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const emp_service = require('./services/employeeServices')
 let id = 1234
 console.log(`Leave Count for ${id} is ${emp_service.getLeaveCount(id)}`)
 
-let gender = "Male"
+let gender = "Female"
 console.log(`Count of  ${gender} Employees in Organization is ${emp_service.getEmployeesByGenderCount(gender)}`)
